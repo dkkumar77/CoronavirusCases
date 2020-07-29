6e89e1e1c9274fb1837ce9cbecf5046f3249dbca
Deepak Kumar <61252338+dkkumar77@users.noreply.github.com>
2020-07-29 00:00:38 -0400

    Update Parse.py
    added title

--- Parse.py
+++ Parse.py
@@ -3,16 +3,19 @@ import requests
 
 
 page = requests.get("https://www.worldometers.info/coronavirus/country/us/")
-tree = html.fromstring(page.content)
-currentStats = tree.xpath('//*[@id="maincounter-wrap"]/div/span/text()')
+treeStat = html.fromstring(page.content)
+currentStats = treeStat.xpath('//*[@id="maincounter-wrap"]/div/span/text()')
 
 worldPage = requests.get("https://www.worldometers.info/coronavirus/?utm_campaign=homeAdvegas1?%22")
-tree1 = html.fromstring(worldPage.content)
+treeStatWorld = html.fromstring(worldPage.content)
 
-currentWorldStats = tree1.xpath('//*[@id="maincounter-wrap"]/div/span/text()')
-print("US CASES")
+currentWorldStats = treeStatWorld.xpath('//*[@id="maincounter-wrap"]/div/span/text()')
 desc = ["Cases", "Deaths", "Recoveries"]
 
+
+print("Current Number of Coronavirus Cases:\n\n\n")
+
+print("US CASES")
 i = 0
 while i<3:
     print(desc[i], currentStats[i])
@@ -24,5 +27,6 @@ i = 0
 while i<3:
     print(desc[i], currentWorldStats[i])
     i+=1
+
 
 
